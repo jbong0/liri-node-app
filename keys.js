6a0c1f346285f02812b0d40989c52a918dcd193a
Jessie Ramirez <jessieramirez@Jessies-MacBook-Air.local>
2018-04-28 13:28:20 -0700

    Initial Files

---
+++ keys.js
@@ -0,0 +1,13 @@
+console.log('this is loaded');
+
+exports.twitter = {
+  consumer_key: process.env.h3RkfhWhTJyfT4ZKW9H1Z4Njw,
+  consumer_secret: process.env.29nJHufjXeBqdtAFSPPaJKIC5ZHqlPM9BxZ4rrlru5nv5UWFob,
+  access_token_key: process.env.990321005997391872-VCZvFaI8gjlAc7eJkRkWUKTeAEsoqL7,
+  access_token_secret: process.env.mpGy0dzD1Hw9HReFEV3tbiYaYazQBa9CDNf4EL8t5imvR,
+};
+
+exports.spotify = {
+  id: process.env.f4b7197a6fdb4e2d8528b3039d67d146,
+  secret: process.env.653c1b5ecaeb4ec5bda28416c5dedcf9
+};
